838168e98fd8df4695447add71e8dbe97237a6db
Adam Letizia <43392371+koalaty-code@users.noreply.github.com>
2021-11-11 13:38:06 -0600

    fix: publish multi-platfrom docker image with docker build-push-action (#50)

--- scripts/publish-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-docker push basistheory/azure-keyvault-emulator:latest
-docker push basistheory/azure-keyvault-emulator:$1
--- scripts/tag-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-docker image tag basistheory/azure-keyvault-emulator basistheory/azure-keyvault-emulator:latest
-docker image tag basistheory/azure-keyvault-emulator basistheory/azure-keyvault-emulator:$1
