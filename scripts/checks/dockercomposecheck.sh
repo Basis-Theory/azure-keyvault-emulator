e2a1edd23b9edf2482a1ecc89d8317d517306ef9
Matthew Trisoline <mstrisoline@users.noreply.github.com>
2024-10-29 09:22:59 -0400

    feat: update secrets and workflows (#147)
    * feat: update secrets and workflows
    
    * update docker compose cli

--- scripts/checks/dockercomposecheck.sh
+++ scripts/checks/dockercomposecheck.sh
@@ -5,7 +5,7 @@ echo "Checking Docker Compose..."
 EXPECTED_COMPOSE_REGEX=^[1-9][0-9]*\.[0-9]+\.[0-9]+
 DISPLAY_COMPOSE_REGEX=$(sed -e 's|\\\([.+?*()]\)|\1|g' -e 's|[.+?]\*|*|g' <<<${EXPECTED_COMPOSE_REGEX})
 
-version=$(docker-compose version --short 2>&1)
+version=$(docker compose version --short 2>&1)
 
 if [[ "$version" =~ $EXPECTED_COMPOSE_REGEX ]]; then
     echo "Docker Compose version \"${version}\" (matching required \"${DISPLAY_COMPOSE_REGEX}\") is OK"
